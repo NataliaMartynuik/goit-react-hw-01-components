48c53f9d8d226b968d61c66d20875b4c393b3145
NataliaMartynuik <98057592+NataliaMartynuik@users.noreply.github.com>
2022-10-12 19:38:59 +0300

    1

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,18 +1,18 @@
 import PropTypes from "prop-types";
-import { Wraper, Online, TextName } from './FriendListItem.styled';
+import { Wraper, Online, TextName, UserPhoto } from './FriendListItem.styled';
 
-export const FriendListItem = ({ friends: avatar, name, isOnline }) => {
+export const FriendListItem = ({ isOnline, name, avatar }) => {
     return (
         <Wraper>
             <Online style={{ backgroundColor: isOnline ? 'green' : 'red' }}>{isOnline}</Online>
-            <img src={avatar} alt="User avatar" width="48" />
+            <UserPhoto src={avatar} alt={name} width="48" />
             <TextName>{name}</TextName>
         </Wraper>
     )
 }
 
 FriendListItem.propTypes = {
-    friends: PropTypes.shape({
+  friends: PropTypes.shape({
     avatar: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
     isOnline: PropTypes.bool.isRequired,
